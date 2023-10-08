1214b64ec8b88b93c0cbf1e27f1faa3d7d5504ed
RuslanHkmoff <ruslan.khackimov2016@gmail.com>
2023-10-08 23:08:50 +0300

    fixed codestyle

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -3,17 +3,18 @@ package edu.hw1;
 public class Task8 {
     private static final int LENGTH = 8;
     private static final int[][] SHIFTS = new int[][] {{-1, -2}, {-2, -1}, {1, 2}, {2, 1}};
+    private static final String EXCEPTION_MESSAGE = "The board must be non null and have size 8x8";
 
     private Task8() {
     }
 
     public static boolean knightBoardCapture(int[][] board) {
         if (board == null || board.length != LENGTH) {
-            throw new IllegalArgumentException("The board must be non null and have size 8x8");
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         }
         for (int i = 0; i < LENGTH; ++i) {
             if (board[i] == null || board[i].length != LENGTH) {
-                throw new IllegalArgumentException("The board must be non null and have size 8x8");
+                throw new IllegalArgumentException(EXCEPTION_MESSAGE);
             }
             for (int j = 0; j < LENGTH; ++j) {
                 if (board[i][j] == 1) {
