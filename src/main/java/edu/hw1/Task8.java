f0f7bf4c807376b7c56123d637b2ebee7fb1434d
RuslanHkmoff <ruslan.khackimov2016@gmail.com>
2023-10-09 14:04:39 +0300

    added constant for knight

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -3,6 +3,7 @@ package edu.hw1;
 public class Task8 {
     private static final int LENGTH = 8;
     private static final int[][] SHIFTS = new int[][] {{-1, -2}, {-2, -1}, {1, 2}, {2, 1}};
+    private static final int KNIGHT = 1;
     private static final String EXCEPTION_MESSAGE = "The board must be non null and have size 8x8";
 
     private Task8() {
@@ -17,12 +18,12 @@ public class Task8 {
                 throw new IllegalArgumentException(EXCEPTION_MESSAGE);
             }
             for (int j = 0; j < LENGTH; ++j) {
-                if (board[i][j] == 1) {
+                if (board[i][j] == KNIGHT) {
                     for (int[] shift : SHIFTS) {
                         if (i + shift[0] > 0 && i + shift[0] < LENGTH
                             && j + shift[1] > 0 && j + shift[1] < LENGTH
                             && board[i + shift[0]] != null && board[j + shift[1]] != null
-                            && board[i + shift[0]][j + shift[1]] == 1) {
+                            && board[i + shift[0]][j + shift[1]] == KNIGHT) {
                             return false;
                         }
                     }
