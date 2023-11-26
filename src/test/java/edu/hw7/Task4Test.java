408cff47a065f4ce17f5b79f58c79f937a68bd62
RuslanHkmoff <ruslan.khackimov2016@gmail.com>
2023-11-26 21:50:42 +0300

    fixed test

--- src/test/java/edu/hw7/Task4Test.java
+++ src/test/java/edu/hw7/Task4Test.java
@@ -2,24 +2,33 @@ package edu.hw7;
 
 import edu.hw7.task4.LinearMonteCarlo;
 import edu.hw7.task4.MultiThreadingMonteCarlo;
-import java.util.concurrent.ExecutionException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Task4Test {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @Test
     @DisplayName("test speed")
     void test() {
         long start = System.currentTimeMillis();
+        double multithreading = 0;
         try {
-            MultiThreadingMonteCarlo.calculate(100_000_000);
+            multithreading = MultiThreadingMonteCarlo.calculate(100_000_000);
         } catch (ExecutionException | InterruptedException e) {
             throw new RuntimeException(e);
         }
         long middle = System.currentTimeMillis();
-        LinearMonteCarlo.calculate(100_000_000);
+        double linear = LinearMonteCarlo.calculate(100_000_000);
         long end = System.currentTimeMillis();
+        LOGGER.info(String.format("Multithreading calculate value: %f, time: %dms", multithreading, (middle - start)));
+        LOGGER.info(String.format("Linear calculate value: %f, time: %dms", linear, (end - middle)));
         assertTrue((middle - start) <= (end - middle));
     }
 }
